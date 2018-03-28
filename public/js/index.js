bf286ecf395fa86b6d0106f9bb710e62d0231c2d
Hridoy4 <Hridoy4@DESKTOP-9CDLVUQ>
2018-03-28 13:15:23 +0600

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -44,10 +44,13 @@ socket.on('disconnect', function () {
 
     e.preventDefault();
 
+    var messageTextbox= jQuery('[name=message]');
+
     socket.emit('createMessage',{
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextbox.val()
     },function () {
+    messageTextbox.val('')
 
     });
   });
@@ -59,12 +62,17 @@ var locationButton = jQuery('#send-location');
       if(!navigator.geolocation) {
         return alert('Geo location not supported by your browser');
       }
+
+      locationButton.attr('disabled','disabled').text('Sending location');
+
       navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage',{
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         });
       }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
       });
     });
